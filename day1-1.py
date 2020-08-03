9a4e1d78c512ff6209d7236d6d341b2f9b45c5c5
Lawson <69131815+lawson0628@users.noreply.github.com>
2020-08-03 11:14:20 +0800

    Add files via upload
    廖彥驊

---
+++ day1-1.py
@@ -0,0 +1,3 @@
+print("I'm Eric")
+print('He is "cool"')
+print('I\'m Eric\nHe is "cool"')
---
+++ day1-2.py
@@ -0,0 +1,4 @@
+Name1=input("please type in your English name:")
+print(Name1)
+Name2=input("請輸入你的中文名字:")
+print('Hello'+Name1+Name2+' 我是LAWSON廖彥驊')
