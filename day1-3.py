e5e50464cbb99ac75821b65006771315f94e19c4
Lawson <69131815+lawson0628@users.noreply.github.com>
2020-08-03 11:55:46 +0800

    Add files via upload

---
+++ day1-3.py
@@ -0,0 +1,12 @@
+ans1=input('請輸入圓的半徑')
+print('圓周長是'+ str(float(ans1)*2*3.14))
+print('圓面積是'+ str(float(ans1)*float(ans1)*3.14))
+
+ans2=input('請輸入你的年齡')
+print('你會在'+ str(float(ans2)+32)+'歲死亡')
+
+ans3=input('請輸入你上次段考的英文成績')
+print('你這次段考會考'+ str(float(ans3)+15)+'分')
+
+ans4=input('請輸入你幾歲時結婚')
+print('你將會在'+ str(float(ans4)+5)+'歲生小孩')
